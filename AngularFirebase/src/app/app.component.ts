7103f8378ee52ff3d1a861cc3786f23d92cbdef0
Mona Lisa <leloizenai@gmail.com>
2020-10-08 22:29:08 +0700

    Integrate Angular Firebase Example

---
+++ AngularFirebase/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'loizenai.com';
+  description = 'Angular-Firebase Demo';
+ 
+  itemValue = '';
+  items: Observable<any[]>;
+ 
+  constructor(public db: AngularFireDatabase) {
+    this.items = db.list('items').valueChanges();
+  }
+ 
+  onSubmit() {
+    this.db.list('/items').push({ content: this.itemValue });
+    this.itemValue = '';
+  }
+}
---
+++ AngularFirebase/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+ 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+ 
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule, // for database
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
