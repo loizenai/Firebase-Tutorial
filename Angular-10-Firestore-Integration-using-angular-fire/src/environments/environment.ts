207d214a26288890af76973e5110260fdef3cf94
Mona Lisa <leloizenai@gmail.com>
2020-10-13 23:20:39 +0700

    Integrate Angular 10 Firestore using @angular/fire

---
+++ Angular-10-Firestore-Integration-using-angular-fire/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBmeT3HEMFfjNmho1F5liOclLkjBNKJrmU",
+    authDomain: "loizenai-firebase.firebaseapp.com",
+    databaseURL: "https://loizenai-firebase.firebaseio.com",
+    projectId: "loizenai-firebase",
+    storageBucket: "loizenai-firebase.appspot.com",
+    messagingSenderId: "887328809823"
+  }
+};
